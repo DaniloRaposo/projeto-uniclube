72618aa1a950008babed99e9fca8000813ab04a1
Danilo Raposo <daniloraposocaldas@hotmail.com>
2025-02-12 21:03:49 -0300

    Home (feat): add category list into home

--- src/Pages/Home/Home.svelte
+++ src/Pages/Home/Home.svelte
@@ -1,15 +1,28 @@
 <script lang="ts">
   import Carousel from "../../Components/Carousel/Carousel.svelte";
-  // images that will be displayed in carousel
+  import CategoryList from "../../Components/CategoryList/CategoryList.svelte";
+  import type { TCategoryItem } from "../../Components/CategoryList/types";
+  /**
+   * images that will be displayed in carousel
+   */
   export let images: Array<string> = [];
+  /**
+   * List of categories
+   */
+  export let listCategories: TCategoryItem[] = [];
 </script>
 
 <div class="container">
   <Carousel {images} />
+  <CategoryList {listCategories} />
 </div>
 
 <style lang="scss">
   .container {
     margin: 15%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 1.25rem;
   }
 </style>
--- src/Pages/Home/HomeViewModel.svelte
+++ src/Pages/Home/HomeViewModel.svelte
@@ -4,6 +4,32 @@
   import Image2 from "../../assets/image2.webp";
 
   const images = [Image1, Image2];
+  const listCategories = [
+    {
+      icon: "iconoir:glasses",
+      label: "Oticas",
+    },
+    {
+      icon: "tabler:paw",
+      label: "Pets",
+    },
+    {
+      icon: "iconoir:glasses",
+      label: "Oticas",
+    },
+    {
+      icon: "tabler:paw",
+      label: "Pets",
+    },
+    {
+      icon: "iconoir:glasses",
+      label: "Oticas",
+    },
+    {
+      icon: "tabler:paw",
+      label: "Pets",
+    },
+  ];
 </script>
 
-<Home {images} />
+<Home {images} {listCategories} />
