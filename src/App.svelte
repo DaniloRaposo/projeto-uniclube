d72f3c97e9958950437c68b42eb611558ab8e4d1
Danilo Raposo <daniloraposocaldas@hotmail.com>
2025-02-16 16:33:27 -0300

    App (feat): adjust header to app

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,80 @@
 <script lang="ts">
   import Router from "svelte-spa-router";
   import Header from "./Components/Header/Header.svelte";
+  import Image1 from "./assets/riocenter.webp";
+  import Image2 from "./assets/ecologica.webp";
+  import type {
+    TStore,
+    TDepartment,
+  } from "./Components/Header/components/types";
 
   import routes from "./routes";
+
+  const listStore: TStore[] = [
+    { image: Image1, label: "Rio Center" },
+    { image: Image2, label: "Ecologica" },
+  ];
+
+  const listDepartments: TDepartment[] = [
+    {
+      name: "Saúde e Bem-estar",
+      subdepartments: ["Beleza", "Cuidados Pessoais", "Esportes e Aventuras"],
+      lastItemType: "see-more",
+    },
+    {
+      name: "Estilo",
+      subdepartments: ["Vestuário", "Calçados", "Bijuteria e Acessórios"],
+      lastItemType: "see-more",
+    },
+    {
+      name: "Casa",
+      subdepartments: [
+        "Casa, Jardim e Limpeza",
+        "Móveis e Decoração",
+        "Construção",
+      ],
+      lastItemType: "see-more",
+    },
+    {
+      name: "Tincidut",
+      subdepartments: ["Tempor", "Consequat"],
+      lastItemType: "see-more",
+    },
+    {
+      name: "Eletro",
+      subdepartments: [
+        "Eletrodomésticos",
+        "Celulares, Tablets, ...",
+        "Câmeras e Acessórios",
+      ],
+      lastItemType: "see-more",
+    },
+    {
+      name: "Ipsum Lorem",
+      subdepartments: ["Sit Amet", "Consequat", "Pellentesque"],
+      lastItemType: "see-more",
+    },
+    {
+      name: "Mais Departamentos",
+      subdepartments: [
+        "Petshop",
+        "Produtos Regionais",
+        "Brinquedos e Jogos",
+        "Livros",
+        "Música e Hobbies",
+        "Bebês",
+        "Alimentos e Bebidas",
+        "Agro e Industria",
+      ],
+      lastItemType: "all-departments",
+    },
+  ];
 </script>
 
 <main>
   <div class="router-container">
     <div class="header">
-      <Header />
+      <Header {listStore} {listDepartments} />
     </div>
     <div class="content">
       <Router {routes} />
