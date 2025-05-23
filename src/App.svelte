b85728606bcae188c683a4df1a3abc523381476c
Danilo Raposo <daniloraposocaldas@hotmail.com>
2025-05-23 08:49:59 -0300

    Home (feat): add header into app test 1

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,33 @@
 <script lang="ts">
   import Router from "svelte-spa-router";
+  import Header from "./Components/Header/Header.svelte";
 
   import routes from "./routes";
 </script>
 
 <main>
-  <Router {routes} />
+  <div class="router-container">
+    <div class="header">
+      <Header />
+    </div>
+    <div class="content">
+      <Router {routes} />
+    </div>
+  </div>
 </main>
+
+<style lang="scss">
+  .router-container {
+    width: 100%;
+    height: 100%;
+    position: relative;
+
+    .header {
+      width: 100%;
+      height: fit-content;
+      position: sticky;
+      top: 0;
+      z-index: 1000;
+    }
+  }
+</style>
